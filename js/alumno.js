93420c599372da59378d064e69f3fcb47e534754
jaortizr <64172165+jaortizr@users.noreply.github.com>
2020-05-06 17:57:54 -0500

    CRUD v1d
    Operaciones 'Editar' y 'Cambiar contraseña' del alumno.

--- js/alumno.js
+++ js/alumno.js
@@ -1,4 +1,76 @@
 $(document).ready(function(){
     $('.tabs').tabs();
     $('.fixed-action-btn').floatingActionButton();
+
+    var rango = [2000,2020];
+    $('.datepicker').datepicker({
+        format:"yyyy-mm-dd",
+        autoClose:true,
+        minDate:new Date(2000,0,1),
+        maxDate:new Date(2020,11,31),
+        yearRange:rango,
+        i18n:{
+            months:['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
+            monthsShort:["Ene","Feb","Mar","Abr","May","Jun","Jul","Ago","Sep","Oct","Nov","Dic"],
+            weekdays:["Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"],
+            weekdaysShort:["Dom","Lun","Mar","Mie","Jue","Vie","Sab"],
+            weekdaysAbbrev:["D","L","M","M","J","V","S"]
+        }
+    });
+
+    $("#formEditAlumno").validetta({
+        bubblePosition: "bottom",
+        bubbleGapTop: 10,
+        bubbleGapLeft: -5,
+        onValid:function(e){
+            e.preventDefault();
+            $.ajax({
+                url:"./alumnoEditar_AX.php",
+                method:"POST",
+                data:$("#formEditAlumno").serialize(),
+                cache:false,
+                success:function(respAX){
+                    var AX = JSON.parse(respAX);
+                    var titulo = "<h2>CRUD - TW - 20202</h2>";
+                    $.alert({
+                        title:titulo,
+                        content:AX.msj,
+                        icon:"fas fa-cogs fa-2x",
+                        theme:"supervan",
+                        onDestroy:function(){
+                            location.reload();
+                        }
+                    });
+                }
+            });
+        }
+    });
+
+    $("#formCambiarContrasena").validetta({
+        bubblePosition: "bottom",
+        bubbleGapTop: 10,
+        bubbleGapLeft: -5,
+        onValid:function(e){
+            e.preventDefault();
+            $.ajax({
+                url:"./alumnoContrasena_AX.php",
+                method:"POST",
+                data:$("#formCambiarContrasena").serialize(),
+                cache:false,
+                success:function(respAX){
+                    var AX = JSON.parse(respAX);
+                    var titulo = "<h2>CRUD - TW - 20202</h2>";
+                    $.alert({
+                        title:titulo,
+                        content:AX.msj,
+                        icon:"fas fa-cogs fa-2x",
+                        theme:"supervan",
+                        onDestroy:function(){
+                            location.reload();
+                        }
+                    });
+                }
+            });
+        }
+    });
 });
